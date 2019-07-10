4fd0a1e342c979315459af5eba960825a6d3b558
Mohamed W. Mehrez <mehrezenator@gmail.com>
2019-07-09 20:05:51 -0400

    codes for MPC for trajectory tracking
    Trajectory tracking implementation

---
+++ workshop_github/Codes_casadi_v3_4_5/MPC_code/Sim_4_MPC_Robot_tracking_mul_shooting.m
@@ -0,0 +1,149 @@
+% Trajectory Tracking + Multiple shooting
+clear all
+close all
+clc
+
+% CasADi v3.4.5
+addpath('C:\Users\mehre\OneDrive\Desktop\CasADi\casadi-windows-matlabR2016a-v3.4.5')
+import casadi.*
+
+T = 0.5; %[s]
+N = 8; % prediction horizon
+rob_diam = 0.3;
+
+v_max = 0.6; v_min = -v_max;
+omega_max = pi/4; omega_min = -omega_max;
+
+x = SX.sym('x'); y = SX.sym('y'); theta = SX.sym('theta');
+states = [x;y;theta]; n_states = length(states);
+
+v = SX.sym('v'); omega = SX.sym('omega');
+controls = [v;omega]; n_controls = length(controls);
+rhs = [v*cos(theta);v*sin(theta);omega]; % system r.h.s
+
+f = Function('f',{states,controls},{rhs}); % nonlinear mapping function f(x,u)
+U = SX.sym('U',n_controls,N); % Decision variables (controls)
+%P = SX.sym('P',n_states + n_states);
+P = SX.sym('P',n_states + N*(n_states+n_controls));
+% parameters (which include the initial state and the reference along the
+% predicted trajectory (reference states and reference controls))
+
+X = SX.sym('X',n_states,(N+1));
+% A vector that represents the states over the optimization problem.
+
+obj = 0; % Objective function
+g = [];  % constraints vector
+
+Q = zeros(3,3); Q(1,1) = 1;Q(2,2) = 1;Q(3,3) = 0.5; % weighing matrices (states)
+R = zeros(2,2); R(1,1) = 0.5; R(2,2) = 0.05; % weighing matrices (controls)
+
+st  = X(:,1); % initial state
+g = [g;st-P(1:3)]; % initial condition constraints
+for k = 1:N
+    st = X(:,k);  con = U(:,k);
+    %obj = obj+(st-P(4:6))'*Q*(st-P(4:6)) + con'*R*con; % calculate obj
+    obj = obj+(st-P(5*k-1:5*k+1))'*Q*(st-P(5*k-1:5*k+1)) + ...
+              (con-P(5*k+2:5*k+3))'*R*(con-P(5*k+2:5*k+3)) ; % calculate obj
+    % the number 5 is (n_states+n_controls)
+    st_next = X(:,k+1);
+    f_value = f(st,con);
+    st_next_euler = st+ (T*f_value);
+    g = [g;st_next-st_next_euler]; % compute constraints
+end
+% make the decision variable one column  vector
+OPT_variables = [reshape(X,3*(N+1),1);reshape(U,2*N,1)];
+
+nlp_prob = struct('f', obj, 'x', OPT_variables, 'g', g, 'p', P);
+
+opts = struct;
+opts.ipopt.max_iter = 2000;
+opts.ipopt.print_level =0;%0,3
+opts.print_time = 0;
+opts.ipopt.acceptable_tol =1e-8;
+opts.ipopt.acceptable_obj_change_tol = 1e-6;
+
+solver = nlpsol('solver', 'ipopt', nlp_prob,opts);
+
+args = struct;
+
+args.lbg(1:3*(N+1)) = 0;  % -1e-20  % Equality constraints
+args.ubg(1:3*(N+1)) = 0;  % 1e-20   % Equality constraints
+
+args.lbx(1:3:3*(N+1),1) = -20; %state x lower bound % new - adapt the bound
+args.ubx(1:3:3*(N+1),1) = 20; %state x upper bound  % new - adapt the bound
+args.lbx(2:3:3*(N+1),1) = -2; %state y lower bound
+args.ubx(2:3:3*(N+1),1) = 2; %state y upper bound
+args.lbx(3:3:3*(N+1),1) = -inf; %state theta lower bound
+args.ubx(3:3:3*(N+1),1) = inf; %state theta upper bound
+
+args.lbx(3*(N+1)+1:2:3*(N+1)+2*N,1) = v_min; %v lower bound
+args.ubx(3*(N+1)+1:2:3*(N+1)+2*N,1) = v_max; %v upper bound
+args.lbx(3*(N+1)+2:2:3*(N+1)+2*N,1) = omega_min; %omega lower bound
+args.ubx(3*(N+1)+2:2:3*(N+1)+2*N,1) = omega_max; %omega upper bound
+%----------------------------------------------
+% ALL OF THE ABOVE IS JUST A PROBLEM SET UP
+
+
+% THE SIMULATION LOOP SHOULD START FROM HERE
+%-------------------------------------------
+t0 = 0;
+x0 = [0 ; 0 ; 0.0];    % initial condition.
+xs = [1.5 ; 1.5 ; 0.0]; % Reference posture.
+
+xx(:,1) = x0; % xx contains the history of states
+t(1) = t0;
+
+u0 = zeros(N,2);        % two control inputs for each robot
+X0 = repmat(x0,1,N+1)'; % initialization of the states decision variables
+
+sim_tim = 30; % Maximum simulation time
+
+% Start MPC
+mpciter = 0;
+xx1 = [];
+u_cl=[];
+
+% the main simulaton loop... it works as long as the error is greater
+% than 10^-6 and the number of mpc steps is less than its maximum
+% value.
+main_loop = tic;
+while(mpciter < sim_tim / T) % new - condition for ending the loop
+    current_time = mpciter*T;  %new - get the current time
+    % args.p   = [x0;xs]; % set the values of the parameters vector
+    %----------------------------------------------------------------------
+    args.p(1:3) = x0; % initial condition of the robot posture
+    for k = 1:N %new - set the reference to track
+        t_predict = current_time + (k-1)*T; % predicted time instant
+        x_ref = 0.5*t_predict; y_ref = 1; theta_ref = 0;
+        u_ref = 0.5; omega_ref = 0;
+        if x_ref >= 12 % the trajectory end is reached
+            x_ref = 12; y_ref = 1; theta_ref = 0;
+            u_ref = 0; omega_ref = 0;
+        end
+        args.p(5*k-1:5*k+1) = [x_ref, y_ref, theta_ref];
+        args.p(5*k+2:5*k+3) = [u_ref, omega_ref];
+    end
+    %----------------------------------------------------------------------    
+    % initial value of the optimization variables
+    args.x0  = [reshape(X0',3*(N+1),1);reshape(u0',2*N,1)];
+    sol = solver('x0', args.x0, 'lbx', args.lbx, 'ubx', args.ubx,...
+        'lbg', args.lbg, 'ubg', args.ubg,'p',args.p);
+    u = reshape(full(sol.x(3*(N+1)+1:end))',2,N)'; % get controls only from the solution
+    xx1(:,1:3,mpciter+1)= reshape(full(sol.x(1:3*(N+1)))',3,N+1)'; % get solution TRAJECTORY
+    u_cl= [u_cl ; u(1,:)];
+    t(mpciter+1) = t0;
+    % Apply the control and shift the solution
+    [t0, x0, u0] = shift(T, t0, x0, u,f);
+    xx(:,mpciter+2) = x0;
+    X0 = reshape(full(sol.x(1:3*(N+1)))',3,N+1)'; % get solution TRAJECTORY
+    % Shift trajectory to initialize the next step
+    X0 = [X0(2:end,:);X0(end,:)];
+    mpciter
+    mpciter = mpciter + 1;
+end;
+main_loop_time = toc(main_loop);
+average_mpc_time = main_loop_time/(mpciter+1)
+
+Draw_MPC_tracking_v1 (t,xx,xx1,u_cl,xs,N,rob_diam)
+
+
